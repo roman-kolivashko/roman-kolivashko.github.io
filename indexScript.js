b9d00c7972920d02692313be79a38047daba9193
Hywie Martin <HywieMartin@hotmail.com>
2016-01-22 14:42:17 +0000

    help command now works | error displayed for invalid command word

--- indexScript.js
+++ indexScript.js
@@ -11,7 +11,7 @@ $(document).ready( function(){
 * - Decides on what function to call depedning on the primary key given.
 */
 function proccessCommand(){
-    
+    getKeyWord($("#commandLine").val());
     appendNewCmdObject();
 }
 
@@ -37,4 +37,45 @@ function appendNewCmdObject(){
             proccessCommand();
         }
     });
+}
+
+/*
+* - Removes first word from command string
+* - Compares word to valid internal command words.
+*/
+function getKeyWord(cmdString){
+    var cmdWord = cmdString.split(" ")[0];
+    
+    switch (cmdWord) {
+        case 'help':
+            helpCommand();
+            break;
+        
+        default:
+            disMessage(cmdWord+": command not found", true);
+    }
+    
+    console.log(cmdWord);
+}
+
+/*
+* - Displays string to terminal.
+* - False or True determins if a break is added at the end | False by default
+*/
+function disMessage(messageToDisplay, hasBreak){
+    if(hasBreak === null){
+        hasBreak = false;
+    }
+    
+    $("#terminalContainer").append( "<p>"+messageToDisplay+"</p>");
+    if(hasBreak){
+       $("#terminalContainer").append( "<br>"); 
+    }
+}
+
+
+function helpCommand(){
+    disMessage("<p> Hywie Martins Bash, Version 0.1.0 </P>", true);
+    disMessage("<p> These shell commands are defined internally. Type '<i>help</i>' to see this list.</P>", true);
+    disMessage("<p> | help | ls | cd | mkdir | date | cp | mv</P>", true);
 }
